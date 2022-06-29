ddcc4a59071220e89116d54d8ec636d57915014d
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-06-29 09:56:02 +0300

    contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.7",
 };
